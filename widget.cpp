4cf7ac623c7679e889605eac83a77b954dafbe1c
TryNotOverheat <nothardme@gmail.com>
2021-12-12 21:14:18 +0300

    V 0.2
    now area calculating works

--- widget.cpp
+++ widget.cpp
@@ -18,7 +18,7 @@ bool Widget::GetValueFromItem(QTableWidgetItem *item, double &value){
     if (item != NULL){
         QString s1 = item->text();
         value = s1.toDouble(&ok);
- }
+    }
     return ok;
 }
 
@@ -43,22 +43,41 @@ void Widget::on_resultButton_clicked()
 {
     int n = ui->tableWidget->rowCount();
     double sum1 = 0, sum2 = 0;
-    for(int r = 0; r < n - 1; r++){
+    for(int r = 0; r < n; r++){
         bool ok = 1;
-        double XCord, YCord;
-        ok = GetValueFromItem(ui->tableWidget->item(r,0), XCord) && ok;
-        ok = GetValueFromItem(ui->tableWidget->item(r + 1,1), YCord) && ok;
+        double XCord = 0, YCord = 0;
+        if (r == n - 1){
+            ok = GetValueFromItem(ui->tableWidget->item(r,0), XCord) && ok;
+            ok = GetValueFromItem(ui->tableWidget->item(0,1), YCord) && ok;
+        }
+        else{
+            ok = GetValueFromItem(ui->tableWidget->item(r,0), XCord) && ok;
+            ok = GetValueFromItem(ui->tableWidget->item(r + 1,1), YCord) && ok;
+        }
         if(ok){
             sum1 = sum1 + XCord * YCord;
         }
+        else{
+            break;
+        }
     }
-    for(int r = 0; r < n - 1; r++){
+    ui->label->setText(ui->label->text() + "\n");
+    for(int r = 0; r < n; r++){
         bool ok = 1;
-        double XCord, YCord;
-        ok = GetValueFromItem(ui->tableWidget->item(r + 1,0), XCord) && ok;
-        ok = GetValueFromItem(ui->tableWidget->item(r,1), YCord) && ok;
+        double XCord = 0, YCord = 0;
+        if (r == n - 1){
+            ok = GetValueFromItem(ui->tableWidget->item(0,0), XCord) && ok;
+            ok = GetValueFromItem(ui->tableWidget->item(r,1), YCord) && ok;
+        }
+        else{
+            ok = GetValueFromItem(ui->tableWidget->item(r + 1,0), XCord) && ok;
+            ok = GetValueFromItem(ui->tableWidget->item(r,1), YCord) && ok;
+        }
         if(ok){
             sum2 = sum2 + XCord * YCord;
+        }
+        else{
+            break;
         }
     }
     double area = abs(sum1 - sum2)/2;
